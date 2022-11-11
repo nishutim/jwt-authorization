5d8892471b98fa7459b2af580ffeb96a78878b2c
nishutim <nikmitush12345@gmail.com>
2022-11-11 15:33:08 +0300

    Render optimization with React.memo

--- src/components/GlobalError/GlobalError.tsx
+++ src/components/GlobalError/GlobalError.tsx
@@ -5,10 +5,10 @@ interface Props {
    error: string
 }
 
-const GlobalError: FC<Props> = ({ error }) => {
+const GlobalError: FC<Props> = React.memo(({ error }) => {
    return (
       <StyledGlobalError>{error}</StyledGlobalError>
    );
-}
+})
 
 export default GlobalError;
--- src/components/Preloader/Preloader.tsx
+++ src/components/Preloader/Preloader.tsx
@@ -1,10 +1,10 @@
 import React from "react";
 import { StyledPreloader } from "./style";
 
-const Preloader = () => {
+const Preloader = React.memo(() => {
    return (
       <StyledPreloader>Loading...</StyledPreloader>
    );
-}
+})
 
 export default Preloader;
